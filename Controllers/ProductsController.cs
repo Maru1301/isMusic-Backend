53d74af1f6c64a18eb7e21e6af1db06225575719
Hung08 <sun60703@gmail.com>
2023-03-15 19:49:39 +0800

    Revert "購物車搜尋功能修改"
    This reverts commit d929e0cf1d6fb93f7b18ceaf7ae118a685ae6058.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -113,20 +113,17 @@ namespace api.iSMusic.Controllers
         [Route("ProductSearch/{words}")]
         public IEnumerable<ProductIndexDTO> GetProductSearch([FromRoute] string words , [FromQuery] string Sort)
         {
-            var data = _db.Products
+            if (Sort == "歌手")
+            {
+
+                var data = _db.Products
                     .Include(product => product.Album)//.ThenInclude(album => album.AlbumType)
                     .Include(product => product.Album.AlbumType)
                     .Include(product => product.Album.AlbumGenre)
                     .Include(product => product.Album.MainArtist)
-                    .Where(product => product.Status == true);
-
-            data = (Sort == "歌手"
-                    ? data.Where(product => product.Album.MainArtist!.ArtistName.Contains(words))
-                    : (Sort=="專輯"
-                        ? data.Where(product => product.Album.AlbumName.Contains(words))
-                        : data));
-
-            var res= data.OrderByDescending(product => product.Album.Released)
+                    .Where(product => product.Status == true)
+                    .Where(product => product.Album.MainArtist!.ArtistName.Contains(words))
+                    .OrderByDescending(product => product.Album.Released)
                     .Select(product => new ProductIndexDTO
                     {
                         Id = product.Id,
@@ -137,8 +134,30 @@ namespace api.iSMusic.Controllers
                         stock = product.Stock,
                         status = product.Status,
                     }).ToList();
-            
-            return res;
+                return data;
+            }
+            else
+            {
+                var data = _db.Products
+                    .Include(product => product.Album)//.ThenInclude(album => album.AlbumType)
+                    .Include(product => product.Album.AlbumType)
+                    .Include(product => product.Album.AlbumGenre)
+                    .Include(product => product.Album.MainArtist)
+                    .Where(product => product.Status == true)
+                    .Where(product => product.Album.AlbumName.Contains(words))
+                    .OrderByDescending(product => product.Album.Released)
+                    .Select(product => new ProductIndexDTO
+                    {
+                        Id = product.Id,
+                        CategoryName = product.ProductCategory.CategoryName,
+                        ProductPrice = product.ProductPrice,
+                        AlbumInfo = product.Album.ToInfoVM(),
+                        productName = product.ProductName,
+                        stock = product.Stock,
+                        status = product.Status,
+                    }).ToList();
+                return data;
+            }
         }
 
 
